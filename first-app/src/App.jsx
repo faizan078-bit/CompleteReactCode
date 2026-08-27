caca87a08200bb25d76977cd8382ee950e69d822
Faizan <faizanshaik9886@gmail.com>
2026-08-27 15:52:54 +0530

    Second commit

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return(
     <>
       <h1>Welcome to react</h1>
+      <h2>Thank you</h2>
       <p>React makes it easier to build websites where the page needs to update dynamically without reloading the entire page.</p>
       
        <About />
